592e5a099a79f74ada59e8417c927206ba0fa8be
ismail <ismail.abb.pro@gmail.com>
2022-06-26 23:54:33 +0000

    Commit

---
+++ src/main/java/ma/emsi/ebankingbackend/dtos/AccountOperationDTO.java
@@ -0,0 +1,16 @@
+package ma.emsi.ebankingbackend.dtos;
+
+import lombok.Data;
+import ma.emsi.ebankingbackend.enums.OperationType;
+
+import java.util.Date;
+
+
+@Data
+public class AccountOperationDTO {
+    private Long id;
+    private Date operationDate;
+    private double amount;
+    private OperationType type;
+    private String description;
+}
---
+++ src/main/java/ma/emsi/ebankingbackend/services/BankService.java
@@ -0,0 +1,37 @@
+package ma.emsi.ebankingbackend.services;
+
+import ma.emsi.ebankingbackend.entities.BankAccount;
+import ma.emsi.ebankingbackend.entities.CurrentAccount;
+import ma.emsi.ebankingbackend.entities.SavingAccount;
+import ma.emsi.ebankingbackend.repositories.BankAccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class BankService {
+    @Autowired
+    private BankAccountRepository bankAccountRepository;
+    public void consulter(){
+        BankAccount bankAccount=
+                bankAccountRepository.findById("18d7d2e7-4615-4883-9ebc-5f546afdce25").orElse(null);
+        if (bankAccount!=null) {
+            System.out.println("********************");
+            System.out.println(bankAccount.getId());
+            System.out.println(bankAccount.getBalance());
+            System.out.println(bankAccount.getStatus());
+            System.out.println(bankAccount.getCreatedAt());
+            System.out.println(bankAccount.getCustomer().getName());
+            System.out.println(bankAccount.getClass().getSimpleName());
+            if (bankAccount instanceof CurrentAccount) {
+                System.out.println("Over Draft=>" + ((CurrentAccount) bankAccount).getOverDraft());
+            } else if (bankAccount instanceof SavingAccount) {
+                System.out.println("Rate=>" + ((SavingAccount) bankAccount).getInterestRate());
+            }
+            bankAccount.getAccountOperations().forEach(op -> {
+                System.out.println(op.getType() + "\t" + op.getAmount() + "\t" + op.getAmount());
+            });
+        }
+    }
+}
